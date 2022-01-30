79eeab84fb660feb57a3279236c476351864dd4e
Sebabin <sebastiancrespin279@gmail.com>
2022-01-29 20:50:55 -0700

    server and sequelize sync up and working

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS my_leads;
+
+CREATE DATABASE my_leads;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const routes = require('./controllers');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+sequelize.sync({force:false}).then(() => {
+    app.listen(PORT, () => console.log(`now listening on ${PORT}`));
+}).catch(err => {
+    console.log(err);
+});
