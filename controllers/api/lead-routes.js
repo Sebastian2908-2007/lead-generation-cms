cc8e6cdfb8c28b69cef5cd890eed87880f8d4d25
Sebabin <sebastiancrespin279@gmail.com>
2022-01-30 18:31:57 -0700

    lead routes are all complete

--- controllers/api/lead-routes.js
+++ controllers/api/lead-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const {Lead,Note} = require('../../models');
 
+// get all leads
 router.get('/',(req,res) => {
     Lead.findAll({
         attributes:['id','first_name','last_name','email','phone_number','created_at'],
@@ -15,6 +16,84 @@ router.get('/',(req,res) => {
         console.log(err);
         res.status(500).json(err);
     })
+});
+
+router.get('/:id',(req,res) => {
+    Lead.findOne({
+        where: {
+            id: req.params.id 
+        },
+        attributes:['id','first_name','last_name','email','phone_number','created_at'],
+        include:{
+            model: Note
+        }
+    }).then(dbLead => {
+        if(!dbLead) {
+            res.status(404).json({message:'no lead with that id'});
+            return;
+        }
+        res.json(dbLead);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
+// create the lead
+router.post('/',(req,res) => {
+    Lead.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        phone_number: req.body.phone_number
+    })
+    .then(dbLead => {
+        if(!dbLead) {
+            res.json({message:'incomplete field'});
+            return;
+        }
+        res.json(dbLead);
+    })
+    .catch(err => { 
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.put('/:id',(req,res) => {
+    Lead.update(req.body,{
+        where: {
+            id: req.params.id 
+        }
+    }).then(dbLead => {
+        if(!dbLead) {
+            res.status(404).json({message:'no lead with that id'});
+            return;
+        }
+        res.json(dbLead);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+router.delete('/:id',(req,res) => {
+    Lead.destroy({
+        where: {
+            id: req.params.id 
+        }
+    }).then(dbLead => {
+        if(!dbLead) {
+            res.status(404).json({message:'no lead with that id'});
+            return;
+        }
+        res.json(dbLead);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+    
 });
 
 module.exports = router;
