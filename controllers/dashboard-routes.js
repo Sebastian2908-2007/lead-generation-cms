7d34dcc7df5ab046a076b10a30795d96a3a5eefa
Sebabin <sebastiancrespin279@gmail.com>
2022-01-31 21:04:43 -0700

    night commit have dashboard form set up

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const {Lead,Note} = require('../models');
+const authorize = require('../utils/authorize');
 
-router.get('/',(req,res) => {
+router.get('/',authorize,(req,res) => {
     res.render('dashboard');
 });
 
