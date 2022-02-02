31db6d691047ea782239cbb68e93f0ca0bcbef8a
Sebabin <sebastiancrespin279@gmail.com>
2022-02-01 22:31:42 -0700

    can now add new lead from dashboard

---
+++ public/javascript/add-lead.js
@@ -0,0 +1,26 @@
+async function addLeadHandler(event) {
+   const first_name = document.querySelector('#lead-first-name').value;
+   const last_name = document.querySelector('#lead-last-name').value;
+   const email = document.querySelector('#email').value;
+   const phone_number = document.querySelector('#phone-number').value;
+   const response = await fetch('/api/leads', {
+       method: 'POST',
+       body: JSON.stringify({
+           first_name,
+           last_name,
+           email,
+           phone_number
+       }),
+       headers: {
+           'Content-Type': 'Application/json'
+       }
+   })
+   if(response.ok) {
+       location.reload();
+       return;
+   }else{
+       alert(response.statusText);
+   }
+
+};
+document.querySelector('.new-lead-form').addEventListener('submit',addLeadHandler);
