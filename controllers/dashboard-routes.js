33169f3ef25428b50ef3912c0b80e5e046020dba
Sebabin <sebastiancrespin279@gmail.com>
2022-02-01 22:02:47 -0700

    have dashboard rendering with my lead data

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,7 +3,19 @@ const {Lead,Note} = require('../models');
 const authorize = require('../utils/authorize');
 
 router.get('/',authorize,(req,res) => {
-    res.render('dashboard');
+    Lead.findAll({
+        attributes:['id','first_name','last_name','email','phone_number','created_at'],
+        include: {
+            model: Note
+        }
+    }).then(dbLeadData => {
+        const leads = dbLeadData.map(lead => lead.get({plain: true}));
+        res.render('dashboard',{leads, loggedIn: req.session.loggedIn});
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+  
 });
 
 module.exports = router;
