fbe8e322c12aca267e694fce06cea7c42c3c9f9e
Sebabin <sebastiancrespin279@gmail.com>
2022-02-08 15:34:47 -0700

    I think I got it this time

--- public/javascript/image-setter.js
+++ public/javascript/image-setter.js
@@ -13,11 +13,13 @@
 function imageHandler () {
    const body = document.querySelector('body'); 
    const location = window.location.toString().split('/')[window.location.toString().split('/').length - 2];
+   const location2 = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
    const home = window.location.hostname
 
 if(location === home) {
-   body.style.backgroundImage = 'https://bestlifeonline.com/wp-content/uploads/sites/3/2016/12/happy-man.jpg?fit=1024%2C753&ssl=1' 
-}else{
+   body.style.backgroundImage = 'https://bestlifeonline.com/wp-content/uploads/sites/3/2016/12/happy-man.jpg?fit=1024%2C753&ssl=1'
+   return; 
+}else if(location !== location2){
    body.style.backgroundImage = 'none' 
 }
 
