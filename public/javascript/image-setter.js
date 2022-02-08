53a4020ec135fb84d11e2758f8df267656957b68
Sebabin <sebastiancrespin279@gmail.com>
2022-02-08 16:09:48 -0700

    heroku test

--- public/javascript/image-setter.js
+++ public/javascript/image-setter.js
@@ -14,16 +14,28 @@ function imageHandler () {
    const body = document.querySelector('body'); 
    const location = window.location.toString().split('/')[window.location.toString().split('/').length - 2];
    const location2 = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
-   const home = window.location.hostname
+   const home = window.location.hostname;
 
-if(location === home) {
-   body.style.backgroundImage = 'https://bestlifeonline.com/wp-content/uploads/sites/3/2016/12/happy-man.jpg?fit=1024%2C753&ssl=1'
-}else if(location2 !== home){
-   body.style.backgroundImage = 'none' 
-}
+// if(home) {
+//    body.style.backgroundImage = 'https://bestlifeonline.com/wp-content/uploads/sites/3/2016/12/happy-man.jpg?fit=1024%2C753&ssl=1'
+// }else{
+//    body.style.backgroundImage = 'none' 
+// }
+
+   switch(home) {
+      case home:
+         body.style.backgroundImage = 'https://bestlifeonline.com/wp-content/uploads/sites/3/2016/12/happy-man.jpg?fit=1024%2C753&ssl=1'
+         break;
+      case home+'/login':
+         body.style.backgroundImage = 'none'
+         break;
+
+
+   }
 
      console.log(location);
-     console.log(window.location.hostname);
+     console.log(window.location.hostname + '/login');
+     console.log(window.location);
 
 };
 
