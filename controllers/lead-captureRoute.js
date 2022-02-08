ab3ebc8ace9bd4d516c02463ad795f38290af792
Sebabin <sebastiancrespin279@gmail.com>
2022-02-08 15:18:37 -0700

    need to deploy this branch to heroku to make sure my code works , somthing with localhost will not work with this code but I think in prodution it will, we shall see

--- controllers/lead-captureRoute.js
+++ controllers/lead-captureRoute.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
 // switched make-20k-per-month from index export file to this route
-router.get('/make-20K-per-month',(req,res) => {
+router.get('/',(req,res) => {
     res.render('lead-capture');
 });
 
 module.exports = router;
+//make-20K-per-month
--- public/javascript/image-setter.js
+++ public/javascript/image-setter.js
@@ -1,13 +1,29 @@
-function imageHandler() {
- const body = document.querySelector('body');   
- const location = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
+// function imageHandler() {
+//  const body = document.querySelector('body');   
+//  const location = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
  
- if(location !== 'make-20K-per-month') {
- body.style.backgroundImage = 'none' 
- }
- else if(location === 'make-20K-per-month'){
-    body.style.backgroundImage = 'https://bestlifeonline.com/wp-content/uploads/sites/3/2016/12/happy-man.jpg?fit=1024%2C753&ssl=1' 
- }
+//  if(location !== 'make-20K-per-month') {
+//  body.style.backgroundImage = 'none' 
+//  }
+//  else if(location === 'make-20K-per-month'){
+//     body.style.backgroundImage = 'https://bestlifeonline.com/wp-content/uploads/sites/3/2016/12/happy-man.jpg?fit=1024%2C753&ssl=1' 
+//  }
+// };
+
+function imageHandler () {
+   const body = document.querySelector('body'); 
+   const location = window.location.toString().split('/')[window.location.toString().split('/').length - 2];
+   const home = window.location.hostname
+
+if(location === home) {
+   body.style.backgroundImage = 'https://bestlifeonline.com/wp-content/uploads/sites/3/2016/12/happy-man.jpg?fit=1024%2C753&ssl=1' 
+}else{
+   body.style.backgroundImage = 'none' 
+}
+
+     console.log(location);
+     console.log(window.location.hostname);
+
 };
 
 imageHandler();
